dcf8756eb5d4e58c858b1225d8f8a34177e72a2c
jonreiter@gmail.com <jonreiter@gmail.com>
2019-03-14 11:53:13 +0800

    tighten up range
    should always return the same set of results now
    also extended query size to get everything

--- trump_tax_cuts.py
+++ trump_tax_cuts.py
@@ -1,4 +1,8 @@
-"""the names of companies which filed fields with certain words in them"""
+"""
+fields related to the tax cuts and jobs act of 2017
+related to periods ending in 2017
+and filed in 2018
+"""
 
 from df_wrappers import facts_stringquery
 
@@ -9,11 +13,12 @@ def main():
     query_string = """
     filingsource:"US SEC" AND
     enddate:[2017-01-01 TO 2017-12-31] AND 
+    filingtime:[2018-01-01 TO 2018-12-31] AND 
     fieldname:"tax cuts and jobs act"
     """
 
     # send off the query
-    resp_data = facts_stringquery(query_string, False)
+    resp_data = facts_stringquery(query_string, False, maxresult=100000)
     # keep unique list of company names from results
     for item in resp_data['hits']:
         print(item['source']['companyname'] +
